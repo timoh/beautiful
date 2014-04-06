ae14ece0e54c02c525b43ae19d16a3fb9bf73ea4
Timo Herttua <timo.herttua@rovio.com>
2014-04-06 11:14:25 +0300

    initial commit coming from legacy project

---
+++ beautifulapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beautifulapp::Application.config.secret_key_base = 'ce3a0e5d84e0456ffc57d8c5081bc4fc813f6e1be01aa52766106731d381b379e5827566077e362d329580bba8ea1bdc831467dd54f7e2de1992d1985fc8ea40'
---
+++ treemap/treemap.js
@@ -0,0 +1,59 @@
+var tm = new $jit.TM.Squarified({  
+  //where to inject the visualization  
+  injectInto: 'infovis',  
+  //parent box title heights  
+  titleHeight: 15,  
+  //enable animations  
+  animate: animate,  
+  //box offsets  
+  offset: 1,  
+  //Attach left and right click events  
+  Events: {  
+    enable: true,  
+    onClick: function(node) {  
+      if(node) tm.enter(node);  
+    },  
+    onRightClick: function() {  
+      tm.out();  
+    }  
+  },  
+  duration: 1000,  
+  //Enable tips  
+  Tips: {  
+    enable: true,  
+    //add positioning offsets  
+    offsetX: 20,  
+    offsetY: 20,  
+    //implement the onShow method to  
+    //add content to the tooltip when a node  
+    //is hovered  
+    onShow: function(tip, node, isLeaf, domElement) {  
+      var html = "<div class=\"tip-title\">" + node.name   
+        + "</div><div class=\"tip-text\">";  
+      var data = node.data;  
+      if(data.playcount) {  
+        html += "play count: " + data.playcount;  
+      }  
+      if(data.image) {  
+        html += "<img src=\""+ data.image +"\" class=\"album\" />";  
+      }  
+      tip.innerHTML =  html;   
+    }    
+  },  
+  //Add the name of the node in the correponding label  
+  //This method is called once, on label creation.  
+  onCreateLabel: function(domElement, node){  
+      domElement.innerHTML = node.name;  
+      var style = domElement.style;  
+      style.display = '';  
+      style.border = '1px solid transparent';  
+      domElement.onmouseover = function() {  
+        style.border = '1px solid #9FD4FF';  
+      };  
+      domElement.onmouseout = function() {  
+        style.border = '1px solid transparent';  
+      };  
+  }  
+});  
+tm.loadJSON(json);  
+tm.refresh();  
